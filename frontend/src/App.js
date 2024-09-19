3037c210499dc67ba75eaa3479c6a3ca7d621bcb
devyadav-00 <dev428845@gmail.com>
2024-09-19 10:31:52 +0530

    created react app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-pretty font-bold text-red-600">dev</h1>
+    </div>
+  );
+}
+
+export default App;
