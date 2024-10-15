49ba8115c0cf3077facbb1988c632a887c4f45ef
devyadav-00 <dev428845@gmail.com>
2024-10-15 16:54:23 +0530

    added backend URL

--- frontend/src/pages/ContactUs.jsx
+++ frontend/src/pages/ContactUs.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import emailjs from "emailjs-com";
+import { toast } from "react-toastify";
+import Loader from "../components/Loader";
 
 const ContactUs = () => {
   const [formData, setFormData] = useState({
@@ -7,41 +9,55 @@ const ContactUs = () => {
     email: "",
     message: "",
   });
+  const [loading, setLoading] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
-
   const handleSubmit = (e) => {
     e.preventDefault();
+    setLoading(true);
+
     const data = {
       to_name: formData.name,
       to_email: formData.email,
       message: formData.message,
     };
-    
-    const serviceId = "service_sbhqtdl";  // Update with your actual EmailJS Service ID
-    const templateId = "template_1d5bepg";  // Update with your actual Template ID
-    const userId = "aU7THG1capnm4eRa2";  // Your EmailJS user ID
 
-    emailjs.send(serviceId, templateId, data, userId)
+    const serviceId = process.env.REACT_APP_SERVICE_ID;
+    const templateId = process.env.REACT_APP_TEMPLATE_ID;
+    const userId = process.env.REACT_APP_USER_ID;
+
+    emailjs
+      .send(serviceId, templateId, data, userId)
       .then(() => {
-        alert("Message Sent Successfully");
+        // alert("Message Sent Successfully");
+        toast.success("Message Sent Successfully");
       })
       .catch((error) => {
         console.error("Email sending error:", error);
-        alert("Failed to send message. Please try again later.");
+        toast.error("Failed to send message. Please try again later.");
+        // alert("Failed to send message. Please try again later.");
+      })
+      .finally(() => {
+        setLoading(false);
       });
 
-    // Clear form after submission
     setFormData({ name: "", email: "", message: "" });
   };
 
   return (
-    <div className="flex flex-col items-center justify-center pt-16 h-[110vh] sm:h-screen bg-gray-100 text-center px-6">
+    <div className="flex flex-col relative items-center justify-center pt-16 h-[110vh] sm:h-screen bg-gray-100 text-center px-6">
+      {loading && (
+        <div className="absolute w-full z-10">
+          <Loader />
+        </div>
+      )}
       <div className="max-w-3xl bg-white   p-8 rounded-xl sm:rounded-lg shadow-md w-full">
-        <h1 className="text-3xl sm:text-4xl font-bold mb-6 text-gray-800">Contact Us</h1>
+        <h1 className="text-3xl sm:text-4xl font-bold mb-6 text-gray-800">
+          Contact Us
+        </h1>
         <p className="sm:text-lg text-gray-700 mb-8">
           Have any questions or feedback? We'd love to hear from you! Fill out
           the form below and we'll get back to you as soon as possible.
